3af7e83def135bf8ab5a6feb59d6d26155d16ffd
joanclarke <jcjuju2014@gmail.com>
2023-11-25 16:27:02 -0500

    set up logic for currentItem, prev/next slides

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -4,17 +4,36 @@ import { FaQuoteRight } from 'react-icons/fa'
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 
 const Carousel = () => {
-  const [people, setPeople] = useState(shortList)
+  const [people, setPeople] = useState(longList)
+  const [currentPerson, setCurrentPerson] = useState(0)
 
-  const prevSlide = () => {}
-  const nextSlide = () => {}
+  const prevSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson - 1 + people.length) % people.length
+      return result
+    })
+  }
+  const nextSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson + 1) % people.length
+      return result
+    })
+  }
 
   return (
     <section>
-      {people.map((person) => {
+      {people.map((person, personIndex) => {
         const { id, image, name, title, quote } = person
         return (
-          <article className="slide" key={id}>
+          <article
+            className="slide"
+            style={{
+              transform: `translateX(${100 * (personIndex - currentPerson)}%)`,
+              opacity: personIndex === currentPerson ? 1 : 0,
+              visibility: personIndex === currentPerson ? 'visible' : 'hidden',
+            }}
+            key={id}
+          >
             <img src={image} alt={name} className="person-img" />
             <h5 className="name">{name}</h5>
             <p className="title">{title}</p>
